57e817575e0bb4005bc5f8017727012a4524c3fb
bazylcossac <bazyl.cossac@gmail.com>
2025-05-30 21:56:39 +0200

    add logo

--- app/index.tsx
+++ app/index.tsx
@@ -8,13 +8,53 @@ import styled from "styled-components/native"
 
 const App = () => {
     return (
-      <View><Text>Hello Worlds</Text></View>
+      <MainContainer>
+        <LogoWrapper>
+          <LogoContainer>
+            <TextLogoFirst>Sac</TextLogoFirst>
+            <TextLogoSecond>ker</TextLogoSecond>
+          </LogoContainer>
+          <LogoContainer>
+            <DescText>expense manager</DescText>
+          </LogoContainer>
+        </LogoWrapper>
+      </MainContainer>
     )
 }
 
 
 
+const MainContainer = styled.View`
+    width: 100%;
+    height: 100%;
+    background-color: white;
+`
 
+const LogoWrapper = styled.View`
+    display: flex;
+    flex-direction: column;
+    padding:40px; 
+    justify-content: center;
+    align-items: center;
+`
+const LogoContainer = styled.View`
+    display: flex;
+    flex-direction: row;
+`
+
+const TextLogoFirst = styled.Text`
+    color: gray;
+    font-size: 30px;
+`
+const TextLogoSecond = styled.Text`
+    color:dodgerblue;
+    font-size: 30px;
+`
+const DescText = styled.Text`
+    font-size:12px;
+    color:gray;
+    opacity: 0.7;
+`
 
 export default App
 
