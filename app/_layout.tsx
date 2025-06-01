57833d9f40687f538dd2c55ff99c273ce8747095
bazylcossac <bazyl.cossac@gmail.com>
2025-06-01 20:12:01 +0200

    add navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,43 +1,43 @@
-import { Stack } from 'expo-router';
-import React from 'react';
-import { Platform, Appearance } from 'react-native';
-import { Colors } from '@/constants/Colors';
+import { Colors } from "@/constants/Colors";
+import { Stack } from "expo-router";
+import React from "react";
+import { Appearance } from "react-native";
 
 export default function TabLayout() {
   const colorScheme = Appearance.getColorScheme();
 
-  const theme = colorScheme === "dark" ? Colors.dark : Colors.light
+  const theme = colorScheme === "dark" ? Colors.dark : Colors.light;
 
   return (
     <Stack
       screenOptions={{
-        headerShown: true,
-        headerStyle: {backgroundColor: theme.backgroundHeader},
-        headerTintColor: theme.text
-
-      }}>
+        headerShown: false,
+        headerStyle: { backgroundColor: theme.backgroundHeader },
+        headerTintColor: theme.text,
+      }}
+    >
       <Stack.Screen
         name="index"
         options={{
-          title: 'Home',
           headerShown: false,
+          title: "Home",
         }}
       />
       <Stack.Screen
         name="explore"
         options={{
           headerShown: true,
-          title: 'Explore',
-          headerTitle: "EEEE"
+          title: "Explore",
+          headerTitle: "EEEE",
         }}
       />
-        <Stack.Screen
-            name="shop"
-            options={{
-                title: 'Shop',
-            }}
-        />
-      <Stack.Screen name="+not-found" options={{headerShown: false}}/>
+      <Stack.Screen
+        name="shop"
+        options={{
+          title: "Shop",
+        }}
+      />
+      <Stack.Screen name="+not-found" options={{ headerShown: false }} />
     </Stack>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,104 +1,130 @@
-  // adb shell am start -n host.exp.exponent/.experience.HomeActivity
-  import React, {useState} from "react"
-  import styled from "styled-components/native"
-  import useUserStore from "../store/store"
-
-
+// adb shell am start -n host.exp.exponent/.experience.HomeActivity
+import { useRouter } from "expo-router";
+import React, { useState } from "react";
+import { Platform, View } from "react-native";
+import styled from "styled-components/native";
+import useUserStore from "../store/store";
 
 const App = () => {
-    const [name, setName] = useState("")
-  // const setUsername = useUserStore(state => state.setUsername)
+  const [name, setName] = useState("");
 
+  const router = useRouter();
+  const setUsername = useUserStore((state) => state.setUsername);
 
-    return (
-      <MainContainer>
-        <LogoWrapper>
-          <LogoContainer>
-            <TextLogoFirst>Sac</TextLogoFirst>
-            <TextLogoSecond>ker</TextLogoSecond>
-          </LogoContainer>
-          <LogoContainer>
-            <DescText>expense manager</DescText>
-          </LogoContainer>
-        </LogoWrapper>
-        <LoginContainer>
+  const handleLogIn = () => {
+    if (name.length > 2) {
+      setUsername(name);
+      setName("");
+      router.push("/explore");
+    }
+  };
+
+  return (
+    <MainContainer>
+      <LogoWrapper>
+        <LogoContainer>
+          <TextLogoFirst>Sac</TextLogoFirst>
+          <TextLogoSecond>ker</TextLogoSecond>
+        </LogoContainer>
+        <LogoContainer>
+          <DescText>expense manager</DescText>
+        </LogoContainer>
+      </LogoWrapper>
+      <View>
+        <StyledKeyboardAvoidingView
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
+          keyboardVerticalOffset={Platform.OS === "ios" ? 60 : 0}
+        >
           {name.length > 2 && <UsernameText>Hello, {name}</UsernameText>}
-            <NameInput placeholder={"Enter your name"} placeholderTextColor="#888" value={name} onChangeText={setName}/>
-            <TouchableOpacity>
-              <ButtonText>Enter</ButtonText>
-            </TouchableOpacity>
-        </LoginContainer>
-      </MainContainer>
-    )
-}
+          <NameInput
+            placeholder={"Enter your name"}
+            placeholderTextColor="#888"
+            value={name}
+            onChangeText={setName}
+          />
+          <TouchableOpacity>
+            <ButtonText onPress={handleLogIn}>Enter</ButtonText>
+          </TouchableOpacity>
+        </StyledKeyboardAvoidingView>
+      </View>
+    </MainContainer>
+  );
+};
 
+const MainContainer = styled.View`
+  width: 100%;
+  height: 100%;
+  background-color: white;
+  position: relative;
+`;
 
+const LogoWrapper = styled.View`
+  top: 0;
+  left: 0;
+  right: 0;
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  padding: 40px;
+  justify-content: center;
+  align-items: center;
+`;
+const LogoContainer = styled.View`
+  display: flex;
+  flex-direction: row;
+`;
+const TextLogoFirst = styled.Text`
+  color: gray;
+  font-size: 30px;
+`;
+const TextLogoSecond = styled.Text`
+  color: dodgerblue;
+  font-size: 30px;
+`;
+const DescText = styled.Text`
+  font-size: 12px;
+  color: gray;
+  opacity: 0.7;
+`;
 
-  const MainContainer = styled.View`
-      width: 100%;
-      height: 100%;
-      background-color: white;
-      position: relative;
-  `
+const LoginContainer = styled.View`
+  display: flex;
+  height: 100%;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 50px;
+  gap: 30;
+`;
 
-  const LogoWrapper = styled.View`
-      position: absolute;
-      top: 0;
-      left: 0;
-      right: 0;
-      display: flex;
-      flex-direction: column;
-      padding:40px; 
-      justify-content: center;
-      align-items: center;
-  `
-  const LogoContainer = styled.View`
-      display: flex;
-      flex-direction: row;
-  `
-  const TextLogoFirst = styled.Text`
-      color: gray;
-      font-size: 30px;
-  `
-  const TextLogoSecond = styled.Text`
-      color:dodgerblue;
-      font-size: 30px;
-  `
-  const DescText = styled.Text`
-    font-size:12px;
-    color:gray;
-    opacity: 0.7;
-`
+const StyledKeyboardAvoidingView = styled.KeyboardAvoidingView`
+  display: flex;
+  height: 100%;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 50px;
+  gap: 30;
+`;
 
-  const LoginContainer = styled.View`
-      display: flex;
-      height: 100%;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-      margin-top: 50px;
-      gap:30
-  `
+const NameInput = styled.TextInput`
+  width: 300px;
+  border-radius: 10px;
+  border: 2px solid rgba(0, 0, 0, 0.3);
+`;
 
-  const NameInput = styled.TextInput`
-      width:300px;
-      border-radius: 10px;
-      border: 2px solid rgba(0,0,0,0.3);
-  `
+const TouchableOpacity = styled.TouchableOpacity`
+  padding: 10px 30px;
+  background-color: #999;
+  border-radius: 10px;
+  border: 2px solid #888;
+`;
+const ButtonText = styled.Text`
+  color: white;
+`;
+const UsernameText = styled.Text`
+  font-weight: bold;
+  color: black;
+`;
 
-  const TouchableOpacity = styled.TouchableOpacity`
-      padding:10px 30px;
-      background-color: #999;
-      border-radius: 10px;
-      border:2px solid #888
-      
-  `
-  const ButtonText = styled.Text`
-      color: white
-  `
-  const UsernameText = styled.Text`
-      font-weight: bold;
-      color:black
-  `
-
-export default App
+export default App;
